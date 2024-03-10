65f1f11e63f9a449d2ec459a3eef3a1252f66fec
ivsonv <vsonv@gmail.com>
2024-03-10 08:57:07 -0300

    add `database.js`, `env`

---
+++ infra/database.js
@@ -0,0 +1,21 @@
+import { Client } from "pg";
+
+export default async function query(queryObject) {
+  const client = new Client({
+    password: process.env.POSTGRES_PASSWORD,
+    database: process.env.POSTGRES_DB,
+    host: process.env.POSTGRES_HOST,
+    port: process.env.POSTGRES_PORT,
+    user: process.env.POSTGRES_USER,
+  });
+
+  try {
+    await client.connect();
+    return await client.query(queryObject);
+  } catch (error) {
+    console.error(error);
+    throw error;
+  } finally {
+    await client.end();
+  }
+}

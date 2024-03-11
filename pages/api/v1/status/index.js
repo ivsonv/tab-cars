7f3444ddcda3102886dd4ce6e278b43e4abb6959
ivsonv <vsonv@gmail.com>
2024-03-11 18:08:33 -0300

    add `api/status`, `test`

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,5 +1,35 @@
 import database from "@/infra/database";
 
-export default function status(request, response) {
-  response.status(200).json({ ok: true });
+export default async function status(request, response) {
+  const openConnections = await _openConnections();
+  const maxConnections = await _maxConnections();
+  const version = await _version();
+
+  response.status(200).json({
+    updated_at: new Date().toISOString(),
+    database: {
+      available_connections: maxConnections - openConnections,
+      open_connections: openConnections,
+      max_connections: maxConnections,
+      version: version,
+    },
+  });
+}
+
+async function _version() {
+  const result = await database.query("SHOW server_version;");
+  return result.rows[0].server_version;
+}
+
+async function _maxConnections() {
+  const result = await database.query("SHOW max_connections;");
+  return Number(result.rows[0].max_connections);
+}
+
+async function _openConnections() {
+  const result = await database.query({
+    text: "SELECT count(*)::int FROM pg_stat_activity WHERE datname = $1",
+    values: [process.env.POSTGRES_DB],
+  });
+  return result.rows[0].count;
 }
--- tests/api/v1/status/get.test.js
+++ tests/api/v1/status/get.test.js
@@ -1,4 +1,17 @@
 test("GET to /api/v1/status should return 200", async () => {
   const rs = await fetch("http://localhost:3000/api/v1/status");
   expect(rs.status).toBe(200);
+
+  const body = await rs.json();
+  expect(body.updated_at).toBeDefined();
+  expect(body.updated_at).toEqual(toDateISO(body.updated_at));
+
+  expect(body.database.available_connections).toEqual(99);
+  expect(body.database.max_connections).toEqual(100);
+  expect(body.database.open_connections).toEqual(1);
+  expect(body.database.version).toEqual("16.2");
 });
+
+function toDateISO(date) {
+  return new Date(date).toISOString();
+}
